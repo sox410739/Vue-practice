de5578fdaa29322893d84a9ed6f12113f342961e
sox410739 <wed12345976@gmail.com>
2021-07-30 01:32:54 +0800

    增加 canvas

--- controller.js
+++ controller.js
@@ -1,10 +1,19 @@
 {
+
     var clock = new Vue({
-        el: '.clock',
+        el: '.screen',
         data: {
-            time: getTimeString()
+            time: getTimeString(),
+            canvas: document.querySelector('#clock'),
         },
-        mounted: updateTime
+        methods: {
+            drawSecondHand,
+        },
+        mounted: updateTime,
+        beforeUpdate() {
+            this.drawSecondHand();
+        }
+
     });
 
 
@@ -50,7 +59,6 @@
      * 網頁載入就 call，隨時更新時間
      */
     async function updateTime() {
-        console.log('good');
         let now = new Date();
         await wait( 1000 - now.getMilliseconds() );
         this.time = getTimeString();
@@ -59,6 +67,22 @@
             await wait(1000);
             this.time = getTimeString();
         }
+    }
+
+    function drawSecondHand() {
+        let second = parseInt(this.time.substring(6));
+
+        let canvas = document.querySelector('#clock');
+        let ctx = canvas.getContext('2d');
+        
+        ctx.beginPath();
+        ctx.moveTo(125,125);
+        ctx.lineTo(125,45);
+        ctx.lineTo(45,125);
+        ctx.closePath();
+        ctx.stroke();
+
+
     }
 
 }
