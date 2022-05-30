d05c331b272d927bd3cc421b9acfa2ccb47959ca
Nameko117 <anniehu117@gmail.com>
2022-05-30 15:34:51 +0800

    update slide

--- controller.js
+++ controller.js
@@ -3,51 +3,51 @@ const vm = new Vue({
     data: {
         videos: [
             {
-                number: 'Enable TVM QNN Flow via BYOC for MediaTek Neuropilot (9900)', display: 'Chun-Ping Chung, Sheng-Yuan Cheng, Chao-Lin Lee, Geng-Ming Liang, Robert Lai, and Jenq-Kuen Lee', id: '4SUZBTFW03g'
+                number: 'Enable TVM QNN Flow via BYOC for MediaTek Neuropilot (9900)', display: 'Chun-Ping Chung, Sheng-Yuan Cheng, Chao-Lin Lee, Geng-Ming Liang, Robert Lai, and Jenq-Kuen Lee', id: '4SUZBTFW03g', slide: '1eBXHf9OxEw6PiCptwOlb55M34-AUhSYG'
             },
             {
-                number: 'A Hybrid Pruning for Accelerating CNN Models on Sparse Tensor Core (5953)', display: 'An-Tai Chen, Pangfeng Liu, Ding-Yong Hong, and Jan-Jan Wu', id: 'gxnTHkyXd4k'
+                number: 'A Hybrid Pruning for Accelerating CNN Models on Sparse Tensor Core (5953)', display: 'An-Tai Chen, Pangfeng Liu, Ding-Yong Hong, and Jan-Jan Wu', id: 'gxnTHkyXd4k', slide: '16HVHPLiBJddRexeVEO6MfEliADZwzRzL'
             },
             {
-                number: 'Identifying Function Positions in Assembly Code with Machine Learning Techniques (8171)', display: 'Jia-Lun Yu and Wuu Yang', id: 'b1_zjnVj9vA'
+                number: 'Identifying Function Positions in Assembly Code with Machine Learning Techniques (8171)', display: 'Jia-Lun Yu and Wuu Yang', id: 'b1_zjnVj9vA', slide: '1xGDvA3T6xehIzTiYCQBHU5AJa7eUFIWF'
             },
             {
-                number: 'Offloading Support for Parallel JavaScript Programs (3133)', display: 'An-Chi Liu and Yi-Ping You', id: 'L2pcNX0oymg'
+                number: 'Offloading Support for Parallel JavaScript Programs (3133)', display: 'An-Chi Liu and Yi-Ping You', id: 'L2pcNX0oymg', slide: null
             },
             {
-                number: 'An Algorithm for Solving the Blocking between Instruction Sink and Division-Modulo Combine (3725)', display: 'Ting-Wei Lin and YungYu Zhuang', id: 'ZQCrrqyYZpA'
+                number: 'An Algorithm for Solving the Blocking between Instruction Sink and Division-Modulo Combine (3725)', display: 'Ting-Wei Lin and YungYu Zhuang', id: 'ZQCrrqyYZpA', slide: null
             },
             {
-                number: 'A GCC-based Compliance Checker for Single-translation-unit Rules in MISRA-C (9200)', display: 'Chih-Yuan Chen, Yung-An Fang, Guan-Ren Wang, and Peng-Sheng Chen', id: 'DT36EoRrBFE'
+                number: 'A GCC-based Compliance Checker for Single-translation-unit Rules in MISRA-C (9200)', display: 'Chih-Yuan Chen, Yung-An Fang, Guan-Ren Wang, and Peng-Sheng Chen', id: 'DT36EoRrBFE', slide: '1pgckMx2P5qPJtEq5VI0PlGVRKzxuAaAn'
             },
             {
-                number: 'Update with Sail Specification for RISC-V (3950)', display: 'Chiahui Su, Chun-Ping Chung, Yu-Tse Huang, Ti-Han Wu, Yu-Wen Shao, Bow-Yaw Wang, Chuanhua Chang, Charlie Su, and Jenq-Kuen Lee', id: 'YxybCdwtx0g'
+                number: 'Update with Sail Specification for RISC-V (3950)', display: 'Chiahui Su, Chun-Ping Chung, Yu-Tse Huang, Ti-Han Wu, Yu-Wen Shao, Bow-Yaw Wang, Chuanhua Chang, Charlie Su, and Jenq-Kuen Lee', id: 'YxybCdwtx0g', slide: '1Fg_BMLlOIqcTDRds7RiyM9alFVsT-Ikl'
             },
             {
-                number: 'A Pruning-Based MCTS Approach to Moldable Job Scheduling on Parallel Computing Platforms (1249)', display: 'Shu-Jun Yang and Kuo-Chan Huang', id: '819UBzj4JJw'
+                number: 'A Pruning-Based MCTS Approach to Moldable Job Scheduling on Parallel Computing Platforms (1249)', display: 'Shu-Jun Yang and Kuo-Chan Huang', id: '819UBzj4JJw', slide: '1qNxQ88u1T_Xsk43GZTrPoBZQHgjXpHF6'
             },
             {
-                number: 'Towards Fine-grained Behavior-based Analysis for Linux CPU Scheduler (6987)', display: 'Hui-Chun Feng, Ching-Chun Huang, and Kuo-Hui Tsai', id: 'wzKjrGCpZaQ'
+                number: 'Towards Fine-grained Behavior-based Analysis for Linux CPU Scheduler (6987)', display: 'Hui-Chun Feng, Ching-Chun Huang, and Kuo-Hui Tsai', id: 'wzKjrGCpZaQ', slide: '14XcMIRCiTgutDIaVXKZxSj-9PgJm5vZE'
             },
             {
-                number: 'An Asynchronous I/O Interface for Scalable and Low-Latency Network Services (1257)', display: 'Yu-Cheng Cheng, Ching-Chun Huang and Chia-Heng Tu', id: 'DQqRgypavO0'
-            },
-            // {
-            //     number: 'Briefing on Pointer-Based Divergence Analysis (2395)', display: 'Shao-Chung Wang, Lin-Ya Yu, Li-An Her, Yuan-Shin Hwang, and Jenq-Kuen Lee', id: ''
-            // },
-            {
-                number: 'Execution Flow Aware Profiling for ROS-based Autonomous Vehicle Software (9391)', display: 'Shaohua Wang, Chia-Heng Tu, and Jyh-Ching Juang', id: 'zzR4TaZnsyQ'
+                number: 'An Asynchronous I/O Interface for Scalable and Low-Latency Network Services (1257)', display: 'Yu-Cheng Cheng, Ching-Chun Huang and Chia-Heng Tu', id: 'DQqRgypavO0', slide: '1Tuj2Wu1zgTMxTPJf47ueIjT01olIDUkf'
             },
             {
-                number: 'Pragmatic Node Size Profiler for Decision Trees (7716)', display: 'Chiahui Su, Chao-Lin Lee, Jenq-Kuen Lee, and Kuan-Hsun Chen', id: 'tWzJT1REW9Y'
+                number: 'Briefing on Pointer-Based Divergence Analysis (2395)', display: 'Shao-Chung Wang, Lin-Ya Yu, Li-An Her, Yuan-Shin Hwang, and Jenq-Kuen Lee', id: null, slide: '1jvuRyQ3ANK_JV9ikSJ_brtv4SRYZzKUu'
+            },
+            {
+                number: 'Execution Flow Aware Profiling for ROS-based Autonomous Vehicle Software (9391)', display: 'Shaohua Wang, Chia-Heng Tu, and Jyh-Ching Juang', id: 'zzR4TaZnsyQ', slide: null
+            },
+            {
+                number: 'Pragmatic Node Size Profiler for Decision Trees (7716)', display: 'Chiahui Su, Chao-Lin Lee, Jenq-Kuen Lee, and Kuan-Hsun Chen', id: 'tWzJT1REW9Y', slide: '17ME0Pe25MCGdGS6kxgSYjOuvkQgfcFgX'
             },
         ],
-        login: true,
+        login: false,
         wrongMessage: false,
         username: '',
         password: '',
         popup: false,
-        main: false,
+        main: true,
         popUrl: '',
         appScriptUrl: 'https://script.google.com/macros/s/AKfycbyP2jWg44KK2BFxdoGNcY8JfwR7uNPRzMeDoD9vlQsP5emIhuEp6dDu0dEc6M-PLxQ1/exec',
     },
@@ -71,6 +71,9 @@ const vm = new Vue({
     // },
 
     methods: {
+        slideNumberClicked(video) {
+            window.open('https://drive.google.com/file/d/' + video.slide + '/view');
+        },
         videoNumberClicked(video) {
             this.popUrl = 'https://www.youtube.com/embed/' + video.id + '?modestbranding=1&autoplay=1&mute=1&disablekb=1';
             this.popup = true;
